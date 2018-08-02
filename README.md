ff84cf86f054ff2792cd842f4dd6d409a02a7ac8
Naimul Haque <naimul011@gmail.com>
2018-08-02 18:59:49 +0600

    Completed

---
+++ README.md
@@ -0,0 +1,1 @@
+# Minimum-Error-Rate-Classifier
---
+++ classifier.m
@@ -0,0 +1,60 @@
+%Author: Naimul Haque 27th Jul, 2018
+clear all;
+clc;
+x1 = -7:.2:7;
+x2 = -7:.2:7;
+[X1,X2] = meshgrid(x1,x2); 
+
+mu1 = [1 1];
+Sigma1 = [.50 .6; .6 1];
+F1 = mvnpdf([X1(:) X2(:)],mu1,Sigma1); 
+F1 = reshape(F1,length(x2),length(x1));
+
+meshc(X1,X2,F1);
+
+axis([-7 7 -7 7 -1.0 .6])
+xlabel('x1'); ylabel('x2'); zlabel('Probability Density');
+hold on;
+
+mu2 = [10 10];
+Sigma2 = [.25 .0; 0 .25];
+F2 = mvnpdf([X1(:) X2(:)],mu2,Sigma2);
+F2 = reshape(F2,length(x2),length(x1));
+
+meshc(X1,X2,F2);
+axis([-7 7 -7 7 -1.0 .6])
+xlabel('x1'); ylabel('x2'); zlabel('Probability Density');
+
+caxis([min(F2(:))-.5*range(F2(:)),max(F2(:))]);
+
+%task 1 and 2
+x = [1 1
+    1 -1
+    4 5
+    -2 2.5
+    0 2
+    2 -3];
+
+g = @(Sigma,mu,n) -log(2*pi)-0.5*log(det(Sigma))-0.5*(x(n,:)'-mu')'*inv(Sigma)*(x(n,:)'-mu')+log(0.5);
+
+for n=1:6
+    g1 = g(Sigma1,mu1,n);
+    g2 = g(Sigma2,mu2,n);
+    
+if g1>g2
+    plot3(x(n,1),x(n,2),-1.0,'cd');
+else
+    plot3(x(n,1),x(n,2),-1.0,'ms');
+end
+end
+
+
+syms x1 x2;
+
+b = @(Sigma,mu) -log(2*pi) - 0.5*log(det(Sigma)) - 0.5*([x1;x2]-mu')'*(Sigma^-1)*([x1;x2]-mu') + log(0.5);
+
+g1 = b(Sigma1,mu1);
+g2 = b(Sigma2,mu2);
+
+g = g1 - g2;
+L3=ezplot(g, [ [-10,10], [-10,10] ]);     
